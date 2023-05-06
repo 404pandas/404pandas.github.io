a26492dc720cbdadb57813a4259b75f3c200d622
404pandas <113853550+404pandas@users.noreply.github.com>
2023-05-06 14:52:09 -0400

    Formatted technologies container. Working on formatting the divs that appear when button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,22 @@ window.addEventListener(
 
 // Tab Javascript
 var instance = M.Tabs.init(el, options);
+
+// On-click display for technologies
+// function showTech1() {
+//   let tech1 = document.getElementById("tech1");
+//   tech1.classList.remove("hidden");
+// }
+
+// function showTech2() {
+//   let tech2 = document.getElementById("tech2");
+//   tech2.classList.remove("hidden");
+// }
+// function showTech3() {
+//   let tech3 = document.getElementById("tech3");
+//   tech3.classList.remove("hidden");
+// }
+// function showTech4() {
+//   let tech4 = document.getElementById("tech4");
+//   tech4.classList.remove("hidden");
+// }
