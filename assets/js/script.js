5304f79e7f50f3888e5706c7086fd7ce8303d702
404pandas <113853550+404pandas@users.noreply.github.com>
2023-05-07 10:27:37 -0400

    Fixed z-index for divs covering buttons and hover effects on them. Moving onto troubleshooting for modal to display on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,23 +11,23 @@ window.addEventListener(
 );
 
 // On-click display for technologies
-// function showTech1() {
-//   let tech1 = document.getElementById("tech1");
-//   tech1.classList.remove("hidden");
-// }
+function showTech1() {
+  let tech1 = document.getElementById("tech1");
+  tech1.classList.remove("hidden");
+}
 
-// function showTech2() {
-//   let tech2 = document.getElementById("tech2");
-//   tech2.classList.remove("hidden");
-// }
-// function showTech3() {
-//   let tech3 = document.getElementById("tech3");
-//   tech3.classList.remove("hidden");
-// }
-// function showTech4() {
-//   let tech4 = document.getElementById("tech4");
-//   tech4.classList.remove("hidden");
-// }
+function showTech2() {
+  let tech2 = document.getElementById("tech2");
+  tech2.classList.remove("hidden");
+}
+function showTech3() {
+  let tech3 = document.getElementById("tech3");
+  tech3.classList.remove("hidden");
+}
+function showTech4() {
+  let tech4 = document.getElementById("tech4");
+  tech4.classList.remove("hidden");
+}
 
 // Messy Panda javascript
 document.addEventListener("DOMContentLoaded", function () {
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Tab Javascript
-let elem = document.querySelector(".tabs");
+let el = document.querySelector(".tabs");
 let options = { swipeable: true };
 var instance = M.Tabs.init(el, options);
 var instance = M.Tabs.getInstance(elem);
