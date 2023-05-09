66e60b77747bc3d696fad6bdd8d68464839700a4
404pandas <113853550+404pandas@users.noreply.github.com>
2023-05-08 20:16:41 -0400

    Finished technology carousel, mostly done with About Me

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,27 +10,17 @@ window.addEventListener(
   false
 );
 
-// On-click display for technologies
-function showTech1() {
-  let tech1 = document.getElementById("tech1");
-  tech1.classList.remove("hidden");
-}
-
-function showTech2() {
-  let tech2 = document.getElementById("tech2");
-  tech2.classList.remove("hidden");
-}
-function showTech3() {
-  let tech3 = document.getElementById("tech3");
-  tech3.classList.remove("hidden");
-}
-function showTech4() {
-  let tech4 = document.getElementById("tech4");
-  tech4.classList.remove("hidden");
-}
+// Close button javascript
+window.onload = function () {
+  document.getElementById("frameworks-close-btn").onclick = function () {
+    this.parentNode.parentNode.remove();
+    return false;
+  };
+};
 
 // Messy Panda javascript
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function (e) {
+  e.preventDefault();
   var Modalelem = document.querySelector(".modal");
   var instance = M.Modal.init(Modalelem);
   instance.open();
@@ -41,3 +31,11 @@ let el = document.querySelector(".tabs");
 let options = { swipeable: true };
 var instance = M.Tabs.init(el, options);
 var instance = M.Tabs.getInstance(elem);
+
+// On-click display for technologies
+function showTech1() {
+  let tech1Container = document.querySelector("#tech1");
+  console.log(tech1Container);
+  tech1Container.classList.remove("hidden");
+  console.log(tech1Container);
+}
